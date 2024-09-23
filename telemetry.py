a9ccef3ac60ad7ddd2b3f82c1a2bf4c5d466dec9
arynrosh <arynrosh@gmail.com>
2024-09-23 08:07:00 -0700

    added new application window

--- telemetry.py
+++ telemetry.py
@@ -1,42 +1,8 @@
+from PyQt5.QtWidgets import QApplication, QWidget
+
 import sys
 
-from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import (
-    QApplication,
-    QCheckBox,
-    QComboBox,
-    QDoubleSpinBox,
-    QLabel,
-    QLineEdit,
-    QListWidget,
-    QMainWindow,
-    QSlider,
-    QSpinBox,
-)
-
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super(MainWindow, self).__init__()
-
-        self.setWindowTitle("My App")
-
 app = QApplication(sys.argv)
-window = MainWindow()
+window = QWidget()
 window.show()
 app.exec()
-
-class MainWindow(QMainWindow):
-
-    def __init__(self):
-        super(MainWindow, self).__init__()
-
-        self.setWindowTitle("My App")
-
-        label = QLabel("Hello")
-        font = widget.font()
-        font.setPointSize(30)
-        widget.setFont(font)
-        widget.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
-
-        self.setCentralWidget(widget)
